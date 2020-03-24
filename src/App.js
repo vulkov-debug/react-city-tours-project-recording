a1417cb358955c5f4c0eda8c1606af25c0db7243
vulkov-debug <computercenter@abv.bg>
2020-03-24 19:21:06 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import TourList from './components/TourList';
+
+function App() {
+  return <React.Fragment>
+    <Navbar/>
+    <TourList />
+  </React.Fragment>;
+}
+
+export default App;
---
+++ src/components/Tour/Tour.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import './tour.scss';
+export default class Tour extends Component {
+    state = {
+        showInfo: false
+    };
+    handleInfo = () => {
+        this.setState({
+            showInfo: !this.state.showInfo
+        })
+    }
+    render() {
+        const {id, city, img, name, info } = this.props.tour;
+        const {removeTour} = this.props;
+        return (
+            <article className='tour'>
+                 <div className='img-container'>
+            <img src={img} alt='tour' />
+            <span className='close-btn' onClick={() => removeTour(id)}>
+                <i className='fas fa-window-close'></i>
+            </span>
+                 </div>
+                 <div className='tour-info'>
+                     <h3>{city}</h3>
+                     <h4>{name}</h4>
+                     <h5>info <span><i className='fas fa-caret-square-down' onClick={this.handleInfo}></i></span></h5>
+                     {this.state.showInfo && <p>{info}</p>}
+                     
+                 </div>
+            </article>
+        )
+    }
+}
